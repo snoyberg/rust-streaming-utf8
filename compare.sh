5a0865bfb7fb4353839b33e2ae26cbcf2900c395
Michael Snoyman <michael@snoyman.com>
2018-07-22 14:05:06 +0300

    Use cargo run

--- compare.sh
+++ compare.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -eux
 
-rustc stream-rs.rs
-./stream-rs < input.txt > output-rs.txt
+cargo run < input.txt > output-rs.txt
 stack stream-hs.hs < input.txt > output-hs.txt
 exec diff output-rs.txt output-hs.txt
